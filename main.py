9a0559e5bfa06fbe8d43df86fd028fffcbda9cd7
ZERO20 <edgargdcv@gmail.com>
2022-07-26 22:58:11 -0500

    feat: added sendgrid to send email

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#   Summary Information                                                       #
+#   Lambda to send a summary information                                      #
+###############################################################################
+
+import os
+from dotenv import load_dotenv
 import pandas as pd
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv()
+
+# SENDGRID
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY', '')
+SENDGRID_SUMMARY_TEMPLATE_ID = os.getenv('SENDGRID_SUMMARY_TEMPLATE_ID', '')
+SENDGRID_FROM_EMAIL = os.getenv('SENDGRID_FROM_EMAIL', '')
 
 
-def read_csv():
+def read_csv() -> pd.DataFrame:
     df = pd.read_csv('summaries_information/summary_information.csv', delimiter=',', skip_blank_lines=True)
     return df
 
 
-def process_data(df):
+def process_data(df: pd.DataFrame) -> dict:
     total_balance = df['Transaction'].sum()
     df['Date'] = pd.to_datetime(df['Date'], format='%m/%d')
     month_transactions = df.groupby(df.Date.dt.month_name())['Transaction'].count()
@@ -30,19 +47,32 @@ def save_db():
     pass
 
 
-def send_mail():
-    pass
+def send_mail(summary_information: dict) -> bool:
+    message = Mail(
+        from_email=SENDGRID_FROM_EMAIL,
+        to_emails='edgargdcv@gmail.com'
+    )
+    message.dynamic_template_data = summary_information
+    message.template_id = SENDGRID_SUMMARY_TEMPLATE_ID
+    try:
+        sg = SendGridAPIClient(SENDGRID_API_KEY)
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+        return True
+    except Exception as e:
+        print(f'An error occurred while sending the summary information: {str(e)}')
+        return False
 
 
 def handler(event=None, context=None):
     data = read_csv()
     summary_information = process_data(data)
     print(summary_information)
+    # save_db()
+    send_mail(summary_information)
 
 
 if __name__ == '__main__':
     handler()
-
-
-
-
